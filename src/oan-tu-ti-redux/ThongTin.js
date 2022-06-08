7915610b3042227382b7c8e4a300d56522c262be
nguyenvi09 <nguyentanvi19942010@gmail.com>
2022-06-08 17:29:53 +0700

    done

--- src/oan-tu-ti-redux/ThongTin.js
+++ src/oan-tu-ti-redux/ThongTin.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 
 class ThongTin extends Component {
   render() {
+    const { soBanThang, soBanThua, soBanHoa, soBanChoi } = this.props;
     return (
       <>
         <h1 className=" display-4 text-warning text-center mb-5 mt-4">
@@ -10,20 +11,18 @@ class ThongTin extends Component {
         </h1>
         <div className="row">
           <div className="col-4" style={{ color: "blue", fontSize: "35px" }}>
-            Thắng:{" "}
-            <span style={{ color: "#19b500" }}>{this.props.soBanThang}</span>
+            Thắng: <span style={{ color: "#19b500" }}>{soBanThang}</span>
           </div>
           <div className="col-4" style={{ color: "blue", fontSize: "35px" }}>
-            Hòa: <span style={{ color: "#bdb3b3" }}>{this.props.soBanHoa}</span>
+            Hòa: <span style={{ color: "#bdb3b3" }}>{soBanHoa}</span>
           </div>
           <div className="col-4" style={{ color: "blue", fontSize: "35px" }}>
-            Thua: <span style={{ color: "red" }}>{this.props.soBanThua}</span>
+            Thua: <span style={{ color: "red" }}>{soBanThua}</span>
           </div>
         </div>
 
         <div style={{ color: "blue", fontSize: "35px" }}>
-          Số bàn chơi:{" "}
-          <span style={{ color: "yellow" }}>{this.props.soBanChoi}</span>
+          Số bàn chơi: <span style={{ color: "yellow" }}>{soBanChoi}</span>
         </div>
       </>
     );
